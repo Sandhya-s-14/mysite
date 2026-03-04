8b0c8d333cd44af039295caee71f590f9512daa6
Sandhya S <Sandhs@adobe.com>
2026-03-04 18:10:35 +0530

    eslint-1

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -1,48 +1,36 @@
 export default async function decorate(block) {
-
   const link = block.querySelector('a');
-
   const resp = await fetch(`${link.href}.json`);
   const json = await resp.json();
-
-  const form = document.createElement("form");
+  const form = document.createElement('form');
 
   json.data.forEach((field) => {
-
-    const label = document.createElement("label");
+    const label = document.createElement('label');
     label.textContent = field.Label;
 
-    const input = document.createElement("input");
+    const input = document.createElement('input');
     input.type = field.Type;
     input.name = field.Name;
-    input.placeholder = field.Placeholder || "";
+    input.placeholder = field.Placeholder || '';
 
     form.append(label);
     form.append(input);
-
   });
 
-  block.textContent = "";
+  block.textContent = '';
   block.append(form);
 
-  // 👇 SUBMIT LOGIC
-  form.addEventListener("submit", async (e) => {
-
+  form.addEventListener('submit', async (e) => {
     e.preventDefault();
 
     const formData = new FormData(form);
-
     const data = Object.fromEntries(formData);
 
-    await fetch("https://script.google.com/macros/s/AKfycbzvtTNzV0yTgMknS4avRRl0b51YPmtM-W_QSSyHfWsBShdH6PzT41yGH-FO1CtO9Y60/exec", {
-      method: "POST",
-      body: JSON.stringify(data)
+    await fetch('https://script.google.com/macros/s/AKfycbzvtTNzV0yTgMknS4avRRl0b51YPmtM-W_QSSyHfWsBShdH6PzT41yGH-FO1CtO9Y60/exec', {
+      method: 'POST',
+      body: JSON.stringify(data),
     });
 
-    alert("Form submitted successfully");
-
     form.reset();
-
   });
-
 }
